b4ebf5555b1dbeff23b36aa78d2f6e0daef870d1
Martin Toman <whomenope@outlook.com>
2019-11-02 13:03:21 +0100

    Walk through target dir

--- main.go
+++ main.go
@@ -30,31 +30,51 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
-	// We instantiate a new repository targeting the given path (the .git folder)
+	// Open the repo
 	repo, err = git.PlainOpen(home + repoDir)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// Walk throufh the dir
+	var ff = func(pathX string, infoX os.FileInfo, errX error) error {
+
+		// first thing to do, check error. and decide what to do about it
+		if errX != nil {
+			fmt.Printf("error 「%v」 at a path 「%q」\n", errX, pathX)
+			return errX
+		}
+
+		// find out if it's a dir or file, if file, print info
+		if !infoX.IsDir() {
+			fmt.Printf("%v\n", pathX)
+			// fmt.Printf("  dir: 「%v」\n", filepath.Dir(pathX))
+			// fmt.Printf("  file name 「%v」\n", infoX.Name())
+			// fmt.Printf("  extenion: 「%v」\n", filepath.Ext(pathX))
+		}
+
+		return nil
+	}
+	err = filepath.Walk(os.Args[1], ff)
+	if err != nil {
+		fmt.Printf("error walking the path %q: %v\n", os.Args[1], err)
+	}
+
 	// Length of the HEAD history
 	fmt.Println("git rev-list HEAD --count")
-
 	// ... retrieving the HEAD reference
 	ref, err := repo.Head()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	// ... retrieves the commit history
 	cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	// ... just iterates over the commits
 	var cCount int
 	err = cIter.ForEach(func(c *object.Commit) error {
@@ -67,30 +87,4 @@ func main() {
 	}
 	fmt.Println(cCount)
 
-	// the function that handles each file or dir
-	var ff = func(pathX string, infoX os.FileInfo, errX error) error {
-
-		// first thing to do, check error. and decide what to do about it
-		if errX != nil {
-			fmt.Printf("error 「%v」 at a path 「%q」\n", errX, pathX)
-			return errX
-		}
-
-		// find out if it's a dir or file, if file, print info
-		if infoX.IsDir() {
-			fmt.Printf("DIR:  %v\n", pathX)
-		} else {
-			fmt.Printf("FILE: %v\n", pathX)
-			// fmt.Printf("  dir: 「%v」\n", filepath.Dir(pathX))
-			// fmt.Printf("  file name 「%v」\n", infoX.Name())
-			// fmt.Printf("  extenion: 「%v」\n", filepath.Ext(pathX))
-		}
-
-		return nil
-	}
-
-	err = filepath.Walk(os.Args[1], ff)
-	if err != nil {
-		fmt.Printf("error walking the path %q: %v\n", os.Args[1], err)
-	}
 }
